62bd800029d89db8542dac55dcdfd8aa26f92d96
TED Vortex <ted.vortex@gmail.com>
2021-07-24 00:19:24 +0200

    build: test full release schedule v2

---
+++ src/release-rules.js
@@ -0,0 +1,12 @@
+const releaseRules = [
+  {type: "build", release: "minor"},
+  {type: "ci", release: "patch"},
+  {type: "docs", release: "minor"},
+  {type: "style", release: "patch"},
+  {type: "refactor", release: "patch"},
+  {type: "test", release: "patch"},
+  {type: "revert", release: "patch"},
+  {type: "chore", release: false},
+];
+
+export default releaseRules;

1fb7068ab4a63a289a3f4574a4a247c9503b44e6
TED Vortex <ted.vortex@gmail.com>
2021-07-24 05:55:37 +0200

    fix: release to docker hub by default

--- release.config.js
+++ release.config.js
@@ -31,14 +31,8 @@ module.exports = {
     [
       "@eclass/semantic-release-docker",
       {
-        "baseImageName": "docker",
+        "baseImageName": process.env.DOCKER_IMAGE,
         "registries": [
-          {
-            "url": "docker.io",
-            "imageName": `docker.io/${process.env.REPO_NAME}`,
-            "user": "DOCKER_USER",
-            "password": "DOCKER_TOKEN"
-          },
           {
             "url": "ghcr.io",
             "imageName": `ghcr.io/${process.env.REPO_OWNER}/${process.env.REPO_NAME}`,

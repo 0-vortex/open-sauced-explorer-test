4474862f215a05eeb84225f6764841e783f802c7
TED Vortex <ted.vortex@gmail.com>
2021-07-24 05:57:18 +0200

    fix: add proper new lines to exported release config

--- release.config.js
+++ release.config.js
@@ -18,7 +18,7 @@ module.exports = {
         "package.json",
         "npm-shrinkwrap.json"
       ],
-      "message": `chore(release): \${nextRelease.version}\\n\\n\${nextRelease.notes}`
+      "message": `chore(release): \${nextRelease.version}\n\n\${nextRelease.notes}`
     }],
     ["@semantic-release/github", {
       "assets": [

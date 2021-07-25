caceda884f4256a4f9b8b9c8111422b9f03a942f
TED Vortex <ted.vortex@gmail.com>
2021-07-25 04:32:18 +0200

    style: add proper release notes for all conventional releases

--- release.config.js
+++ release.config.js
@@ -6,7 +6,22 @@ module.exports = {
       "releaseRules": "./src/release-rules.js"
     }],
     ["@semantic-release/release-notes-generator", {
-      "preset": "conventionalcommits"
+      "preset": "conventionalcommits",
+      "presetConfig": {
+        types: [
+          {type: 'feat', section: 'Features'},
+          {type: 'feature', section: 'Features'},
+          {type: 'fix', section: 'Bug Fixes'},
+          {type: 'perf', section: 'Performance Improvements'},
+          {type: 'revert', section: 'Reverts'},
+          {type: 'docs', section: 'Documentation'},
+          {type: 'style', section: 'Styles'},
+          {type: 'refactor', section: 'Code Refactoring'},
+          {type: 'test', section: 'Tests'},
+          {type: 'build', section: 'Build System'},
+          {type: 'ci', section: 'Continuous Integration'}
+        ]
+      }
     }],
     "@semantic-release/changelog",
     ["@semantic-release/npm", {
